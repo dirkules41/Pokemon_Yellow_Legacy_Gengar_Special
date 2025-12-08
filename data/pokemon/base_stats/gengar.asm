abae54682089db7f9b704d3c083c3efcf3b405a4
dirkules41 <max.liebler.psn@gmail.com>
2025-12-09 00:37:45 +0100

    gengar.asm aktualisieren

--- data/pokemon/base_stats/gengar.asm
+++ data/pokemon/base_stats/gengar.asm
@@ -11,7 +11,7 @@
 	dw GengarPicFront, GengarPicBack
 
 	db CONFUSION, CONFUSE_RAY, NO_MOVE, NO_MOVE ; level 1 learnset
-	db GROWTH_MEDIUM_SLOW ; growth rate
+	db GROWTH_SLOW ; growth rate
 
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
